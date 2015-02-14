3733f911434cc4cec64ba51b7d64b24822deb70e
Matt Harrington <mattharrington@users.noreply.github.com>
2015-02-13 17:06:25 -0800

    Initial commit
    This version was published to the Windows Phone store in 2014.

---
+++ KVMR.WP8/KVMR.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using KVMR.WP8.Resources;
+
+namespace KVMR.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
