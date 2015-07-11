a9643acdfb3697b7b7825058b217e3a3cc81a98d
Matt Harrington <MattHarrington@users.noreply.github.com>
2015-07-11 14:03:10 -0700

    Switched to Xamarin.Forms

---
+++ KVMR/KVMR.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using KVMR.WinPhone.Resources;
+
+namespace KVMR.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
